79d4cbefcfd9cf2ada32537f765054ba34d45df6
Redouane ech-chkernit <chkernnit@gmail.com>
2024-03-30 00:58:02 +0000

    add auth

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('members/', views.members, name='members'),
     path('members/details/<int:id>', views.details, name='details'),
     path('testing/', views.testing, name='testing'), 
+
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.user_register, name='register'),
 ]
